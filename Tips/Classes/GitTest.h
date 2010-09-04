5e32ccdb0d3a7b9382e4651a9268555381455ef1
BrendanGaynor <gaynorbrendan@gmail.com>
2010-09-04 15:36:45 -0400

    Added some classes and amended the project

---
+++ Tips/Classes/GitTest.h
@@ -0,0 +1,16 @@
+//
+//  GitTest.h
+//  Tips
+//
+//  Created by Brendan Gaynor on 9/4/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface GitTest : NSObject {
+
+}
+
+@end
---
+++ Tips/Classes/GitTest.m
@@ -0,0 +1,14 @@
+//
+//  GitTest.m
+//  Tips
+//
+//  Created by Brendan Gaynor on 9/4/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "GitTest.h"
+
+
+@implementation GitTest
+
+@end
