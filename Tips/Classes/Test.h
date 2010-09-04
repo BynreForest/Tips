4c7991ef2fb6e92dd59a5eff0e7848188adff967
BrendanGaynor <gaynorbrendan@gmail.com>
2010-09-04 16:05:54 -0400

    Two File Additions

---
+++ Tips/Classes/Test.h
@@ -0,0 +1,16 @@
+//
+//  Test.h
+//  Tips
+//
+//  Created by Brendan Gaynor on 9/4/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Test : NSObject {
+
+}
+
+@end
---
+++ Tips/Classes/Test.m
@@ -0,0 +1,14 @@
+//
+//  Test.m
+//  Tips
+//
+//  Created by Brendan Gaynor on 9/4/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "Test.h"
+
+
+@implementation Test
+
+@end
